1cb38b0ecc36a3989734fea823584a0c120b469d
__Annie__ <oretagalexis@gmail.com>
2022-03-20 11:15:46 -0600

    2.0

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/Gif-App/' , 
   plugins: [react()],
   test: {
     setupFiles: ['./vitest.setup.js'],
